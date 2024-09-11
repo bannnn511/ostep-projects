32b521f615fcd4e2b5637b487b1c9182b4f5a9aa
An Ha <habinhan1999@gmail.com>
2024-09-11 18:38:29 +0700

    adding lottery scheduler

---
+++ scheduling-xv6-lottery/tests/test_1.c
@@ -0,0 +1,28 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[]) {
+    int rc1 = fork();
+
+    if (rc1 == 0) {
+        sleep(50);
+    } else if (rc1 > 0) {
+        int rc2 = fork();
+        if (rc2 == 0) {
+            sleep(50);
+        } else if (rc2 > 0) {
+            int rc3 = fork();
+            if (rc3 == 0) {
+                sleep(50);
+            }
+        }
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        wait();
+    }
+
+    exit();
+}
